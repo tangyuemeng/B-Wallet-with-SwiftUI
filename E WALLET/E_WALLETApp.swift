bb34751e1ccd3b4ef3ea287741c4a97f01699f87
TANG YUEMENG <tangyuemeng@gmail.com>
2023-02-04 11:03:35 +0800

    Initial Commit

---
+++ E WALLET/E_WALLETApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_WALLETApp.swift
+//  E WALLET
+//
+//  Created by YUEMENG TANG on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct E_WALLETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
